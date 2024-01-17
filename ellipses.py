5802233e0d4f2bdefed366d0dc351a78b0cda6f1
Carlos J. Gil Bellosta <cgb@circiter.es>
2024-01-17 15:50:35 +0100

    Implemented an alternative fitting model

--- ellipses.py
+++ ellipses.py
@@ -3,9 +3,8 @@ import numpy as np
 from PIL import Image, ImageDraw, ImagePath
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
-import math
-import random
-from scipy.optimize import minimize
+
+from mylib import get_random_ellipse, get_ellipse, get_best_ellipse, get_best_ellipse_alt
 
 stroke_color = "#000"
 bg_color = "#fff"
@@ -14,73 +13,6 @@ width = 800
 height = 600
 
 center = (width / 2, height / 2)
-
-
-
-def get_ellipse(center, major, ratio, rotation, n_points):
-    rotation_radians = rotation / 360 * 2 * math.pi
-    rsin, rcos = math.sin(rotation_radians), math.cos(rotation_radians)
-
-    xy = [(math.cos(th)  * major, math.sin(th) * major * ratio )
-        for th in [i * (2 * math.pi) / n_points for i in range(n_points)] ]
-    xy = [(x * rcos - y * rsin, x * rsin + y * rcos) for x, y in xy]
-    xy = [(x + center[0], y + center[1]) for x, y in xy]
-    return xy
-
-
-def get_random_ellipse(center, n_points):
-
-    major = random.uniform(40, 300)
-    ratio = random.uniform(.1, 1)
-    rotation = random.uniform(0, 360)
-
-    xy = get_ellipse(center, major, ratio, rotation, n_points)
-    return xy
-
-
-def get_best_ellipse(points):
-
-    print(points.head())
-
-    d0 = points.diff().fillna(0) ** 2
-    d1 = points.diff(-1).fillna(0) ** 2
-
-    d0 = np.sqrt(d0.x + d0.y)
-    d1 = np.sqrt(d1.x + d1.y)
-
-    d = ((d0 + d1) / 2).to_numpy()
-
-    my_points = points.to_numpy()
-    my_center = ((my_points[:, 0] * d).sum() / sum(d), (my_points[:, 1] * d).sum() / sum(d))
-
-    print(my_center)
-
-    def error(parms):
-        major, ratio, rotation = parms
-        xy = get_ellipse(my_center, major, ratio, rotation, len(points))
-        xy = np.array(xy)
-
-        #print(my_points[0:5, :])
-        #print(xy[0:5, :])
-
-        dists_x = np.subtract.outer(my_points[:, 0], xy[:, 0])
-        dists_y = np.subtract.outer(my_points[:, 1], xy[:, 1])
-        dists = dists_x**2 + dists_y**2
-        out = dists.min(axis=0).sum()
-        return out
-
-
-    out = minimize(error, np.array([300, .5, 45]), method='Nelder-Mead', tol=1e-6)
-
-    #print(out)
-
-    out = {
-        'center': my_center,
-        'major': out['x'][0],
-        'ratio': out['x'][1],
-        'rotation': out['x'][2]
-    }
-    return out
 
 
 st.sidebar.title("Calculate Button Example")
@@ -93,7 +25,6 @@ if st.sidebar.button("Regenerate ellipse"):
 
 if st.sidebar.button("Calculate"):
     st.session_state["drawing"] = False
-    print("hola")
 
 
 # default state
@@ -133,9 +64,12 @@ else:
 
     if not objects.empty:
 
+        # preparing the drawing area
+        res_img  = Image.new("RGB", (width, height), "#ffffff")
+        img1 = ImageDraw.Draw(res_img)
+
+        # getting the coordinates from the drawing path
         x = objects.path.tail(1).values[0]
-
-
 
         tmp = pd.DataFrame(x, columns=['type', 'x0', 'y0', 'x1', 'y1'])
         tmp = tmp[tmp.type == 'Q']
@@ -148,29 +82,26 @@ else:
 
         drawing_coordinates = pd.concat([a, b], ignore_index=True)
 
-        best_ellipse_parms = get_best_ellipse(drawing_coordinates)
-
+        # calculate the best ellipse
+        best_ellipse_parms = get_best_ellipse_alt(drawing_coordinates)
         best_ellipse = get_ellipse(
-            center, best_ellipse_parms['major'],
-            best_ellipse_parms['ratio'], best_ellipse_parms['rotation'], 100)
+            center,
+            best_ellipse_parms['major'],
+            best_ellipse_parms['ratio'],
+            best_ellipse_parms['rotation'], 100)
 
-        #print(best_ellipse)
 
         # best ellipse drawing (red)
-        res_img  = Image.new("RGB", (width, height), "#ffffff")
-        img1 = ImageDraw.Draw(res_img)
         img1.polygon(best_ellipse, outline ="red")
 
         # target ellipse
-        xy = st.session_state["xy"]
-        img1.polygon(xy, outline ="blue")
+        img1.polygon(st.session_state["xy"], outline ="blue")
 
         # actual drawn ellipse
-        tmp = [tuple(x) for x in drawing_coordinates.to_records(index=False)]
-        tmp = [(x + center[0] - best_ellipse_parms['center'][0], y + center[1] - best_ellipse_parms['center'][1])
-            for x, y in tmp
-        ]
-        img1.polygon(tmp, outline ="black")
-        st.image(res_img)
+        tmp = best_ellipse_parms['center']
+        xy = [tuple(x) for x in drawing_coordinates.to_records(index=False)]
+        xy = [(x - tmp[0] + center[0], y - tmp[1] + center[1]) for x, y in xy]
+        img1.polygon(xy, outline ="black")
 
-        #st.dataframe(tmp)
+
+        st.image(res_img)
